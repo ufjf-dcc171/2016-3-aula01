4f099813fb490ef0eff64d82d7dccb37cf84199a
Igor Knop <igorknop@gmail.com>
2016-08-25 22:30:47 -0300

    Altera PrincipalAlternativo para usar coleções

--- src/br/ufjf/dcc171/PrincipalAlternativo.java
+++ src/br/ufjf/dcc171/PrincipalAlternativo.java
@@ -1,14 +1,19 @@
 package br.ufjf.dcc171;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrincipalAlternativo {
 
    public static void main(String[] args) {
-      Livro l1 = new Livro("Java como programar", "João da Silva", 1967, 50, 123.45);
-      Livro l2 = new Livro("Java como programar: o retorno da função", "João da Silva", 1972, 56, 73.45);
-      Livro l3 = new Livro("Java como programar: o bug contra ataca", "João da Silva", 1976, 156, 173.45);
-      System.out.println(l1);
-      System.out.println(l2);
-      System.out.println(l3);
+      List<Livro> livros = new ArrayList<>();
+
+      livros.add(new Livro("Java como programar", "João da Silva", 1967, 50, 123.45));
+      livros.add(new Livro("Java como programar: o retorno da função", "João da Silva", 1972, 56, 73.45));      livros.add(new Livro("Java como programar: o bug contra ataca", "João da Silva", 1976, 156, 173.45));
+
+      for (Livro livro : livros) {
+         System.out.println(livro);
+      }
    }
 
 }
